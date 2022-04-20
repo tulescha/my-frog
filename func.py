8bfafe1fa49b13b2b360f61488f970f269169b17
Tulescha <anisina2005@gmail.com>
2022-04-20 16:43:16 +0500

    обавлены новые скины лягушек

--- func.py
+++ func.py
@@ -5,13 +5,20 @@ from token_bot import *
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn2)
     sent = bot.send_message(message.chat.id,
                             'Привет, {0.first_name}. Рад тебя видеть. Вот твоя первая лягушка!'.format(
                                 message.from_user), reply_markup=markup)
-    bot.send_photo(chat_id=message.chat.id, photo=open('data/start_frog.png', 'rb'))
-    # https://papik.pro/risunki/18647-milye-zhabki-risunki-69-foto.html
+    num_foto = random.randint(1, 4)
+    if num_foto == 1:
+        bot.send_photo(chat_id=message.chat.id, photo=open('data/start_frog.png', 'rb'))
+    elif num_foto == 2:
+        bot.send_photo(chat_id=message.chat.id, photo=open('data/start_frog2.jpg', 'rb'))
+    elif num_foto == 3:
+        bot.send_photo(chat_id=message.chat.id, photo=open('data/start_frog3.jpg', 'rb'))
+    else:
+        bot.send_photo(chat_id=message.chat.id, photo=open('data/start_frog4.jpg', 'rb'))
+    btn = types.KeyboardButton("❓ Задать вопрос")
+    markup.add(btn)
     sent = bot.send_message(message.chat.id, 'Чтобы узнать список команд, нажми на кнопку "❓ Задать вопрос"')
 
 
